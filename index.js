a3cb9b08a4c9e245fd18b05db6cab76f9086edff
Muthusornam <94225877+Muthusornam@users.noreply.github.com>
2022-06-02 20:43:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+
+   setTimeout(() => {
+       document.querySelector("#sec1").innerHTML = 0;
+       document.querySelector("#sec2").innerHTML = 9;
+       setTimeout(() => {
+           document.querySelector("#sec1").innerHTML = 0;
+           document.querySelector("#sec2").innerHTML = 8;
+           setTimeout(() => {
+               document.querySelector("#sec1").innerHTML = 0;
+               document.querySelector("#sec2").innerHTML = 7;
+               setTimeout(() => {
+                   document.querySelector("#sec1").innerHTML = 0;
+                   document.querySelector("#sec2").innerHTML = 6;
+                   setTimeout(() => {
+                       document.querySelector("#sec1").innerHTML = 0;
+                       document.querySelector("#sec2").innerHTML = 5;
+                       setTimeout(() => {
+                           document.querySelector("#sec1").innerHTML = 0;
+                           document.querySelector("#sec2").innerHTML = 4;
+                           setTimeout(() => {
+                               document.querySelector("#sec1").innerHTML = 0;
+                               document.querySelector("#sec2").innerHTML = 3;
+                               setTimeout(() => {
+                                   document.querySelector("#sec1").innerHTML = 0;
+                                   document.querySelector("#sec2").innerHTML = 2;
+                                   setTimeout(() => {
+                                       document.querySelector("#sec1").innerHTML = 0;
+                                       document.querySelector("#sec2").innerHTML = 1;
+                                       setTimeout(() => {
+                                           document.querySelector("#sec1").innerHTML = 0;
+                                           document.querySelector("#sec2").innerHTML = 0;
+                                           setTimeout(() => {
+                                            document.querySelector(".container").style.color= "orange";
+                                               document.querySelector(".container").innerText= "HAPPY INDEPENDENCE DAY!!!";
+                                           }, 500)
+                                       }, 1000)
+                                   }, 1000)
+                               }, 1000)
+                           }, 1000)
+                       }, 1000)
+                   }, 1000)
+               }, 1000)
+           }, 1000)
+       }, 1000)
+   }, 1000)
+
+
+
